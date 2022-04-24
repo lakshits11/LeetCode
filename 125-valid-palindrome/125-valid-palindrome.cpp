4c8f322499b7d7ee8756d0b8a2994c7b6bfc25e9
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-04-24 09:58:00 +0530

    Time: 13 ms (15.90%), Space: 7.3 MB (75.67%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int n = s.length();
         int i=0, j=n-1;
         while(i<=j)
