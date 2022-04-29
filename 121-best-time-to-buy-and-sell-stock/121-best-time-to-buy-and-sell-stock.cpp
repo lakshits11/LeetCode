d2abf46b92e0874abf42673d14c8e4833808c530
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-04-29 11:45:23 +0530

    Time: 125 ms (85.35%), Space: 93.4 MB (53.70%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
     {
         ios::sync_with_stdio(false);
         cin.tie(NULL);
-        cout.tie(NULL);
+        // cout.tie(NULL);
         int maxProfit = 0;
         int minElement = arr[0];
         int n = arr.size();
