e9b395432811e1f81268c5eabb80dcee4940f73d
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-04-30 22:45:01 +0530

    Time: 7 ms (63.63%), Space: 6.9 MB (60.16%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,29 +1,25 @@
-class Solution
-{
+class Solution {
 public:
-    bool dp(int i, int j, string &s, string &p)
-    {
-        bool ans;
-        // If `j` reach end of pattern `p` then it fully matches if and only if `i` reach end of string `s`
-        if (j == p.length())
-            return (i == s.length());
-
-        if (j + 1 < p.length() && p[j + 1] == '*')
-        {
-            ans = dp(i, j + 2, s, p); // match zero chars
-            if (i < s.length() && (s[i] == p[j] || p[j] == '.'))
-                ans = ans || dp(i + 1, j, s, p);
-            return ans;
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        
+        dp[0][0] = true;
+        for(int i = 1; i <= n; ++i)
+            if(p[i-1] == '*') dp[0][i] = dp[0][i-2];
+        for(int i = 1; i <= m; ++i){
+            for(int j = 1; j <= n; ++j){
+                if(p[j-1] == '.')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] != '*')
+                    dp[i][j] = dp[i-1][j-1] && p[j-1] == s[i-1];
+                else if(p[j-2] != '.' && p[j-2] != s[i-1])
+                    dp[i][j] = dp[i][j-2];
+                else
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j] || dp[i][j-2];     
+            }
         }
-        if (p[j] == '.' || i < s.length() && s[i] == p[j])
-            return dp(i + 1, j + 1, s, p);
-
-        return false;
-    }
-
-    bool isMatch(string s, string p)
-    {
-        ios_base::sync_with_stdio(false);cin.tie(nullptr);
-        return dp(0, 0, s, p);
+        
+        return dp[m][n];
     }
 };
