18a515a1ea0c80a3c04174b6b0bba35150dedfc5
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-05-01 01:13:04 +0530

    Time: 3 ms (37.18%), Space: 6.7 MB (5.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         vector<vector<int>> dp(m, vector<int>(n,0));
         for(int i=0;i<n;i++)
         {
