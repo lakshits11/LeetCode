162de0663863572d02a19bb27a7bb6c052af49a2
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-05-04 00:40:50 +0530

    Time: 3 ms (39.44%), Space: 6.2 MB (92.77%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<int> ans(n+1,0);
+        ans[0]=1;
+        for(int i=1;i<=n;++i)
+        {
+            for(int j=i;j>=1;j--)
+                ans[j] += ans[j-1];
+        }
+        return ans;
+    }
+};
