2c74f252e4efe4f0b3feffe2a9f9ca98b6f5fab4
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-05-08 09:51:59 +0530

    Time: 17 ms (66.60%), Space: 13.1 MB (56.47%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
@@ -18,35 +18,34 @@
 
 class NestedIterator {
 public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        begins.push(nestedList.begin());
-        ends.push(nestedList.end());
-    }
-
-    int next() {
-        hasNext();
-        return (begins.top()++)->getInteger();
-    }
-
-    bool hasNext() {
-        while (begins.size()) {
-            if (begins.top() == ends.top()) {
-                begins.pop();
-                ends.pop();
-            } else {
-                auto x = begins.top();
-                if (x->isInteger())
-                    return true;
-                begins.top()++;
-                begins.push(x->getList().begin());
-                ends.push(x->getList().end());
+    
+    vector<int> v;
+    int i = 0;
+    
+    void dfs(const vector<NestedInteger> &nestedList)
+    {
+        for(const auto& x : nestedList)
+        {
+            if(x.isInteger())
+                v.push_back(x.getInteger());
+            else
+            {
+                dfs(x.getList());
             }
         }
-        return false;
     }
-
-private:
-    stack<vector<NestedInteger>::iterator> begins, ends;
+    
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        dfs(nestedList);
+    }
+    
+    int next() {
+        return v[i++];
+    }
+    
+    bool hasNext() {
+        return i<v.size();
+    }
 };
 
 /**
