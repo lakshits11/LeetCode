21a45ba551a0dc830e2db186c618426d7fc34061
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-05-29 20:10:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.70%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        int n = s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='6'){
+                s[i]='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
