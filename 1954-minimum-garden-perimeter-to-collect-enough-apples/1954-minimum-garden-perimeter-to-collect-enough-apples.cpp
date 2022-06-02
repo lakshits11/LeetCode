bcaa78f6b48237aacdc4936d378a93ef97f7a22d
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-02 22:18:29 +0530

    Time: 26 ms (15.73%), Space: 6 MB (31.84%) - LeetHub

--- 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.cpp
+++ 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     long long minimumPerimeter(long long neededApples) {
+        ios_base::sync_with_stdio(0);
         long long n=1, s=0, count=0;
-        while(s < neededApples)
+        while(neededApples > 0)
         {
             n+=1;
-            s += (12*(n-1)*(n-1));
+            neededApples -= (12*(n-1)*(n-1));
             count+=2;
         }
         return 4*count;
