c404a2fcfaf655c0101df348cc04c92e743a921a
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-07 17:13:00 +0530

    Time: 7 ms (9.98%), Space: 7.7 MB (47.48%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
-        int n = nums.size();
+        int n = nums.size(), prev2, prev1, curr;
         if(n==1)    return nums[0];
-        vector<int> dp(n+1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        if(n==2)    return max(nums[0], nums[1]);
+        prev2 = nums[0];
+        prev1 = max(nums[0], nums[1]);
         for(int i=2; i<n; i++)
         {
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+            curr = max(prev2+nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
