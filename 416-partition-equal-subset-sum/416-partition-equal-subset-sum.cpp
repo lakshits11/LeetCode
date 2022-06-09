318b75329bc6fa6cd76359793cfb70549d561486
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-09 11:36:46 +0530

    Time: 12 ms (98.65%), Space: 9.2 MB (93.39%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if(total%2!=0)
-            return false;
-        int target = total/2;
-        
-        bool dp[2][target+1];
-        memset(dp, false, sizeof(dp));
-        dp[0][0] = dp[1][0] = true;
-        
-        if(nums[0]<=target)
-            dp[0][nums[0]] = true;
-        
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<=target;j++)
-            {
-                if(j<nums[i])
-                    dp[(i%2)][j] = dp[!(i%2)][j];
-                else
-                    dp[(i%2)][j] = dp[!(i%2)][j] | dp[!(i%2)][j-nums[i]];
-            }
-        }
-        
-        return dp[0][target];
+        int sum = accumulate(nums.begin(), nums.end(), 0), target = sum >> 1;
+        if (sum & 1) return false;
+        bitset<10001> dp(1);
+
+        for(auto num : nums) 
+            dp = dp | dp<<num;
+        return dp[target];
     }
 };
