09b83a651a14c7abcc76770ccd07f5706ef76dba
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-10 16:28:37 +0530

    Time: 4 ms (98.46%), Space: 9.1 MB (60.73%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,27 +1,37 @@
-class Solution
-{
+class Solution {
 public:
-    int total = 0, n;
-    int f(vector<int> &nums, int i, int sum, int target, vector<vector<int>> &dp)
+    int findWays(vector<int> &arr, int k)
     {
-        if (i == n && sum == target)
-            return 1;
-        if (i == n)
-            return 0;
+        int mod = 1e9+7;
+        int n = arr.size();
+        int dp[k+1];
+        memset(dp, 0, sizeof(dp));
         
-        if (dp[i][sum+total] != -1)
-            return dp[i][sum+total];
-        
-        return dp[i][sum+total] = f(nums, i + 1, sum + nums[i], target, dp) + f(nums, i + 1, sum - nums[i], target, dp);
-    }
+        if(arr[0]==0)
+            dp[0]=2;
+        else
+            dp[0]=1;
 
-    int findTargetSumWays(vector<int> &nums, int target)
-    {
-        n = nums.size();
-        for (int i = 0; i < n; i++){
-            total += nums[i];
+        if(arr[0]!=0 && arr[0]<=k)
+            dp[arr[0]] = 1;
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=k;j>=0;j--)
+            {
+                if(j>=arr[i])
+                    dp[j] = dp[j] + dp[j-arr[i]];
+            }
         }
-        vector<vector<int>> dp(n, vector<int>((2 * total) + 1, -1));
-        return f(nums, 0, 0, target, dp);
+
+        return dp[k];
+    }
+    
+    int findTargetSumWays(vector<int>& arr, int d) {
+        ios_base::sync_with_stdio(0);
+        int total = accumulate(arr.begin(), arr.end(), 0);
+        if (total - d < 0 || (total - d) % 2)
+            return 0;
+        return findWays(arr, (total - d) / 2);
     }
 };
