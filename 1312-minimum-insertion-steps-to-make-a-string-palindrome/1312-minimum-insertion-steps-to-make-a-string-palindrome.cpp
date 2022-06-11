865b69a072d6f063030461df919a27113c51fe7d
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-12 00:13:07 +0530

    Time: 48 ms (66.72%), Space: 27 MB (41.91%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int f(int i, int j, string &s, vector<vector<int>> &mem)
-    {
-        if(i==j || i>j)    return 0;
-        if(mem[i][j]!=-1)
-            return mem[i][j];
-        if(s[i]==s[j])
-            return mem[i][j] = f(i+1, j-1, s, mem);
-        else
-            return mem[i][j] = 1 + min(f(i+1, j, s, mem), f(i, j-1, s, mem));
-    }
+    // int f(int i, int j, string &s, vector<vector<int>> &mem)
+    // {
+    //     if(i==j || i>j)    return 0;
+    //     if(mem[i][j]!=-1)
+    //         return mem[i][j];
+    //     if(s[i]==s[j])
+    //         return mem[i][j] = f(i+1, j-1, s, mem);
+    //     else
+    //         return mem[i][j] = 1 + min(f(i+1, j, s, mem), f(i, j-1, s, mem));
+    // }
     
     int minInsertions(string s) {
         int n = s.length();
-        vector<vector<int>> mem(n, vector<int>(n, -1));
-        return f(0, n-1, s, mem);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s[i]==s[j])
+                    dp[i][j] = dp[i+1][j-1];
+                else
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
     }
 };
