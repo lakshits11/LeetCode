ef84daafed990f820003b38da2f1941d0df1f18d
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-11 23:41:36 +0530

    Time: 16 ms (73.02%), Space: 13.9 MB (41.41%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,30 +1,20 @@
-class Solution {
+class Solution{
 public:
-    
-    unordered_map<int, int> mem;
-    unordered_map<int, int> points;
-    
-    int f(int i)
-    {
-        if(i==0)    return 0;
-        if(i==1)    return points[1];
-        if(mem.find(i)!=mem.end())
-            return mem[i];
-        
-        return mem[i] = max(f(i-1), points[i]+f(i-2));
-    }
-    
-    int deleteAndEarn(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        int n = nums.size(), maxn = INT_MIN;
-        
-        for(int i=0;i<n;i++)
-        {
+    int deleteAndEarn (vector<int> &nums){
+        vector<int> points(10001, 0);
+        int n = nums.size(), maxn = -1;
+        for (int i=0;i<n;i++){
             points[nums[i]]+=nums[i];
             maxn = max(maxn, nums[i]);
         }
+        int twoBack = 0, oneBack = points[1], temp;
+        for (int i = 2; i <= maxn; i++) {
+            temp = oneBack;
+            oneBack = max(twoBack+points[i], oneBack);
+            twoBack = temp;
+        }
         
-        return f(maxn);
+        return oneBack;
         
     }
 };
