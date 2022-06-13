c84b576058803e1318cd800022126a96c0de7dab
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-13 19:43:07 +0530

    Time: 64 ms (63.40%), Space: 6.5 MB (94.49%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -2,32 +2,44 @@ class Solution {
 public:
     bool isMatch(string s2, string s1) {
         ios_base::sync_with_stdio(false);
-        int n = s1.length(), m = s2.length();
-        bool dp[n+1][m+1];
+        int n = s1.length(), m = s2.length(), k;
+        bool prev[m+1], temp[m+1];
+        // prev is 0th row. So anywhere we see dp[0][j] => directly replace it with prev[j]
+        memset(prev, false, sizeof(prev));
+        memset(temp, false, sizeof(temp));
         
-        dp[0][0] = true;
-        for(int j=1;j<=m;j++)
-            dp[0][j] = false;
-        for(int i=1;i<=n;i++)
+        prev[0] = true;
+        
+        for(int j=1;j<=m;++j)   prev[j]=false;
+        
+        // for every row, you are assigning 0th column value
+        /*for(int i=1;i<=n;i++)
         {
             if(s1[i-1]=='*' && dp[i-1][0]==true)
                 dp[i][0]=true;
             else
                 dp[i][0]=false;
-        }
+        }*/
+        
         for(int i=1;i<=n;++i)
         {
+            if(s1[i-1]=='*' && prev[0]==true)
+                temp[0]=true;
+            else
+                temp[0]=false;
             for(int j=1;j<=m;++j)
             {
                 if (s1[i-1] == s2[j-1] || s1[i-1] == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
+                    temp[j] = prev[j - 1];
                 else if(s1[i-1]=='*')
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    temp[j] = prev[j] || temp[j-1];
                 else
-                    dp[i][j] = false;
+                    temp[j] = false;
             }
+            for(k=0;k<=m;++k)
+                prev[k] = temp[k];
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
