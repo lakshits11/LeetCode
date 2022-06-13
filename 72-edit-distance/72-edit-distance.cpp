4b5011a71dd71fa78c43526edc0d47a7de106862
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-13 12:24:03 +0530

    Time: 19 ms (54.99%), Space: 6.4 MB (97.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,25 +3,24 @@ public:
     int minDistance(string s1, string s2) {
         ios_base::sync_with_stdio(0);
         int m = s1.length(), n = s2.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        vector<int> prev(n+1, 0), temp(n+1, 0);
+
+        for(int j=1;j<=n;j++)
+            prev[j]=j;
         
-        for(int i=0;i<=m;i++)
-            dp[i][0]=i;
-        for(int j=0;j<=n;j++)
-            dp[0][j]=j;
         for(int i=1;i<=m;++i)
         {
+            temp[0] = i;
             for(int j=1;j<=n;++j)
             {
                 if(s1[i-1]==s2[j-1])
-                    dp[i][j]= dp[i-1][j-1];
+                    temp[j]= prev[j-1];
                 else
-                    dp[i][j] = 1 + min({
-                    dp[i-1][j], dp[i][j-1], dp[i-1][j-1]
-                });
+                    temp[j] = 1 + min({prev[j], prev[j-1], temp[j-1]});
             }
+            prev = temp;
         }
         
-        return dp[m][n];
+        return prev[n];
     }
 };
