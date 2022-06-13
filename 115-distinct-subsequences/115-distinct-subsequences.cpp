816b8d5512ff0e02ea6dc3ffdb2809c3b57dfaf8
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-13 11:33:49 +0530

    Time: 102 ms (9.87%), Space: 30.3 MB (7.28%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,25 +1,51 @@
+// class Solution
+// {
+// public:
+//     int numDistinct(string s, string t)
+//     {
+//         int n = s.length(), m = t.length();
+//         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+//         for (int i = 0; i <= n; ++i) {
+//             for (int j = 0; j <= m; ++j) {
+//                 if (j == 0)
+//                     dp[i][j] = 1;
+//                 else if (i == 0 && j != 0)
+//                     dp[i][j] = 0;
+//                 else if (s[i - 1] == t[j - 1])
+//                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+//                 else
+//                     dp[i][j] = dp[i - 1][j];
+//             }
+//         }
+
+//         return dp[n][m];
+//     }
+// };
+
+#define ull unsigned long long
 class Solution
 {
 public:
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &mem)
-    {
-        // Base Case: If all characters of s2 are matched, then return 1
-        if (j < 0)
-            return 1;
-        if (i < 0)
-            return 0;
-        if (mem[i][j] != -1)
-            return mem[i][j];
-        if (s1[i] == s2[j])
-            return mem[i][j] = f(i - 1, j, s1, s2, mem) + f(i - 1, j - 1, s1, s2, mem);
-        else
-            return mem[i][j] = f(i - 1, j, s1, s2, mem);
-    }
-
     int numDistinct(string s, string t)
     {
+        ios_base::sync_with_stdio(false);
         int n = s.length(), m = t.length();
-        vector<vector<int>> mem(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, s, t, mem);
+        vector<vector<ull>> dp(n + 1, vector<ull>(m + 1, 0));
+
+        dp[0][0]=1;
+        for (int i = 1; i <= n; ++i)
+        {
+            dp[i][0] = 1;
+            for (int j = 1; j <= m; ++j)
+            {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][m];
     }
 };
