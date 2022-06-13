b891c65efa2c5f48ce482a83932e9a1112ab704c
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-14 00:34:13 +0530

    Time: 160 ms (89.86%), Space: 74.7 MB (99.98%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -6,25 +6,22 @@ typedef long long ll;
 class Solution {
 public:    
     int maxProfit(vector<int>& prices) {
+        ios_base::sync_with_stdio(false);
         int n = prices.size();
-        int dp[n+1][5];
+        int dp[5];
         memset(dp, 0, sizeof(dp));
-        // for(int i=0;i<=4;i++)
-        //     dp[n][i] = 0;
-        // for(int i=0;i<=n;i++)
-        //     dp[i][4]=0;
         
         for(int ind=n-1;ind>=0;ind--)
         {
             for(int tr=3;tr>=0;tr--)
             {
                 if(tr%2==0)
-                    dp[ind][tr] = max(-prices[ind] + dp[ind+1][tr+1], dp[ind+1][tr]);
+                    dp[tr] = max(-prices[ind] + dp[tr+1], dp[tr]);
                 else
-                    dp[ind][tr] = max(prices[ind] + dp[ind+1][tr+1], dp[ind+1][tr]);
+                    dp[tr] = max(prices[ind] + dp[tr+1], dp[tr]);
             }
         }
         
-        return dp[0][0];
+        return dp[0];
     }
 };
