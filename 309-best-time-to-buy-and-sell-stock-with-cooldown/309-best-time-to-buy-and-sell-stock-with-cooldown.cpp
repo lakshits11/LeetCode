2e9bb73cac6ec8fabdacfc30bae788efdd4d2d7c
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-14 16:34:55 +0530

    Time: 7 ms (55.39%), Space: 11.2 MB (88.35%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int dp[n+2][2];
-        memset(dp, 0, sizeof(dp));
+        int curr[2], fut1[2], fut2[2];
+        memset(curr, 0, sizeof(curr));
+        memset(fut1, 0, sizeof(fut1));
+        memset(fut2, 0, sizeof(fut2));
         
         for(int i=n-1;i>=0;i--)
         {
             for(int j=0;j<2;++j)
             {
                 if(j==0)
-                    dp[i][0] = max(dp[i+1][0], -prices[i]+dp[i+1][1]);
+                    curr[0] = max(fut1[0], -prices[i]+fut1[1]);
                 else
-                    dp[i][1] = max(dp[i+1][1], prices[i]+dp[i+2][0]);
+                    curr[1] = max(fut1[1], prices[i]+fut2[0]);
             }
+            /* fut2=fut1
+            fut1=curr
+            */
+            copy(fut1, fut1+2, fut2);
+            copy(curr, curr+2, fut1);
         }
-        return dp[0][0];
+        return curr[0];
     }
 };
