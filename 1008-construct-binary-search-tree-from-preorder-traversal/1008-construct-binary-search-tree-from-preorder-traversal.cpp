0cc548b9aa0b5cf0c4a1425a32f451207ae1a8b3
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-20 00:20:11 +0530

    Time: 7 ms (66.20%), Space: 13.8 MB (47.35%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,33 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-    TreeNode* bstFromPreorder(vector<int>& pr) {
-        int size = pr.size();
-        return buildIt(pr,0,size-1);
-    }
-    TreeNode*buildIt(vector<int>&pr,int left,int right)
+    TreeNode *build(vector<int> &pr, int l, int r)
     {
-        if(left>right)
+        if (l > r)
             return NULL;
-        TreeNode*t = new TreeNode();
-        t->val =  pr[left];
-        int i=left+1;
-        while(i<pr.size() && pr[i]<=pr[left])
+        TreeNode *node = new TreeNode(pr[l]);
+        int i = l + 1;
+        while (i <= r && pr[i] <= pr[l])
             i++;
-        t->left = buildIt(pr,left+1,i-1);
-        t->right = buildIt(pr,i,right);
-        return t;
-        
-        
+        node->left = build(pr, l + 1, i - 1);
+        node->right = build(pr, i, r);
+        return node;
+    }
+
+    TreeNode *bstFromPreorder(vector<int> &pr)
+    {
+        int sz = pr.size();
+        return build(pr, 0, sz - 1);
     }
 };
