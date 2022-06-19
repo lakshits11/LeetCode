23e116342020af55d13bb2dec304e6d01134e494
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-19 19:52:29 +0530

    Time: 8 ms (44.21%), Space: 12.3 MB (99.47%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
@@ -1,37 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    int depth(TreeNode* root)
+    {
+        if(root==NULL)  return 0;
+        return 1 + max(depth(root->left), depth(root->right));
+    }
+    
+    void levelO(vector<vector<int>> &ans, TreeNode* node, int level)
+    {
+        if(node==NULL)  return;
+        ans[level].push_back(node->val);
+        levelO(ans, node->left, level-1);
+        levelO(ans, node->right, level-1);
+    }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
         ios_base::sync_with_stdio(false);
-        vector<vector<int>> ans;
-        if(root==NULL)  return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            vector<int> temp;
-            int qlen = q.size();
-            for(int i=0;i<qlen;i++)
-            {
-                TreeNode* t = q.front();
-                q.pop();
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
-                temp.push_back(t->val);
-            }
-            ans.push_back(temp);
-        }
-        reverse(ans.begin(), ans.end());
+        int d = depth(root);
+        vector<vector<int>> ans(d, vector<int> {});
+        levelO(ans, root, d-1);
         return ans;
     }
 };
