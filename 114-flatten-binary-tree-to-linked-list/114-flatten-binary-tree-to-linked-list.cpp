a777b59da02d0c16b2a673cf3a97c109d77bf1b0
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-23 17:34:53 +0530

    Time: 12 ms (35.69%), Space: 12.9 MB (19.26%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -9,18 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class Solution
+{
 public:
-    TreeNode* prev = NULL;
-    void flatten(TreeNode* root) {
-        // ios_base::sync_with_stdio(false);
-        
-        if(root==NULL)  return;
-        flatten(root->right);
-        flatten(root->left);
-        
-        root->right = prev;
-        root->left = NULL;
-        prev = root;
+    void flatten(TreeNode *root)
+    {
+        if (root == NULL)   return;
+        stack<TreeNode *> st;
+        st.push(root);
+        while (!st.empty())
+        {
+            TreeNode *temp = st.top();
+            st.pop();
+
+            if (temp->right)
+                st.push(temp->right);
+            if (temp->left)
+                st.push(temp->left);
+            
+            if(!st.empty())
+                temp->right = st.top();
+            temp->left = NULL;
+        }
     }
 };
