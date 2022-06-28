e59055256d9d2eb46008b54b1f368963dad10973
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-06-28 16:26:39 +0530

    Time: 3 ms (81.07%), Space: 7.6 MB (78.08%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    void f(int ind, vector<int> &nums, vector<vector<int>> &ans)
+    {
+        if(ind==nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind; i<nums.size();++i) {
+            swap(nums[ind], nums[i]);
+            f(ind+1, nums, ans);
+            swap(nums[ind], nums[i]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        vector<vector<int>> ans;
+        // f(starting index, nums, ans)
+        f(0,nums,ans);
+        return ans;
+    }
+};
