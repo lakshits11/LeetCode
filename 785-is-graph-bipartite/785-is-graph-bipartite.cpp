383e8b3482e13563935fa0409894676e9b4fbeed
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-06 17:40:28 +0530

    14ms (99.91%), 13.4 MB(83.54%)

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,30 +1,31 @@
 class Solution {
+private:
+	bool dfs(int node, vector<vector<int>> &adj, vector<int> &color)
+	{
+		if(color[node]==-1)
+			color[node]=1;
+		for(int x : adj[node])
+		{
+			if(color[x]==-1)
+			{
+				color[x] = !color[node];
+				if(!dfs(x, adj, color))
+					return false;
+			}
+			else if(color[x]==color[node])
+				return false;
+		}
+		return true;
+	}
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        ios_base::sync_with_stdio(false);
-        int n = graph.size();
-        vector<int>color(n,-1);
-        
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                // then we have to check there is no matching color
-                if(!dfs(i,color,graph)) return false;
-            }   
-        }
-        return true;
-    }
-    
-    bool dfs(int node,vector<int>&color,vector<vector<int>>&graph){
-       
-        if(color[node]==-1)color[node] =1;
-        
-        for(auto it:graph[node]){
-            if(color[it]==-1){
-                color[it] = 1- color[node];
-                if(!dfs(it,color,graph))return false;
-            }
-            if(color[it]==color[node]) return false;
-            
+    bool isBipartite(vector<vector<int>>& adj) {
+        int n= adj.size();
+        vector<int> color(n, -1);
+        for(int i=0;i<n;i++)
+        {
+        	if(color[i]==-1)
+        		if(!dfs(i, adj, color))
+        			return false;
         }
         return true;
     }
