9cea5689ddd2eb812462a246228abd1c7ce54793
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-13 09:23:45 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (31.07%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -16,21 +16,23 @@ public:
         if(root==NULL)  return ans;
         queue<TreeNode*> q;
         q.push(root);
+        
         while(!q.empty())
         {
-            int sz = q.size();
-            vector<int> temp;
-            for(int i = 0; i<sz; i++)
-            {   
-                TreeNode* t = q.front();
+            vector<int> t;
+            int qlen = q.size();
+            while(qlen--)
+            {
+                TreeNode* temp = q.front();
                 q.pop();
-                temp.push_back(t->val);
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
+                t.push_back(temp->val);
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
-            ans.push_back(temp);
+            ans.push_back(t);
+            
         }
-        
         return ans;
     }
 };
