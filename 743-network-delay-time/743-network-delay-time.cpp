f1afddb3ec4f3b4471ecf5ce24adc466480f780c
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-19 10:58:53 +0530

    150ms (72%) and 40 MB (57%)

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,22 +1,47 @@
-class Solution {
+// By Lakshit Somani
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 public:
-    int networkDelayTime(vector<vector<int>>& times, int N, int K)
+    int networkDelayTime(vector<vector<int>> &times, int n, int k)
     {
-        ios_base::sync_with_stdio(false);
-        vector<int> dist(N + 1, INT_MAX);
-        dist[K] = 0;
-        for (int i = 0; i < N; i++) {
-            for (vector<int> e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
-                    dist[v] = dist[u] + w;
+        vector<int> dist(n + 1);
+        for (int i = 1; i < n + 1; ++i)
+            dist[i] = INT_MAX;
+        unordered_map<int, vector<pair<int, int>>> graph;
+        for (int i = 0; i < times.size(); ++i)
+            graph[times[i][0]].push_back({times[i][1], times[i][2]});
+        dist[k] = 0;
+        queue<pair<int, int>> q;
+        // q is storing { distance of node from source, node }
+        q.push({0, k});
+        while (!q.empty())
+        {
+            auto temp = q.front();
+            q.pop();
+            for (int i = 0; i < graph[temp.second].size(); ++i)
+            {
+                // for each adjacent node p
+                auto p = graph[temp.second][i];
+                // p.second is distance of p from temp
+                // dist[p.first] is distance of p from src
+                if (temp.first + p.second < dist[p.first])
+                {
+                    dist[p.first] = temp.first + p.second;
+                    q.push({temp.first + p.second, p.first});
                 }
             }
         }
-
-        int maxwait = 0;
-        for (int i = 1; i <= N; i++)
-            maxwait = max(maxwait, dist[i]);
-        return maxwait == INT_MAX ? -1 : maxwait;
+        int val = 0;
+        for (int i = 0; i < dist.size(); ++i)
+        {
+            if (dist[i] == INT_MAX)
+                return -1;
+            else
+                val = max(val, dist[i]);
+        }
+        return val;
     }
 };
