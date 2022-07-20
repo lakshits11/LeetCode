99fcc913c7fe34b05d1698370500723040da3cc1
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-20 12:33:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (45.61%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int maxScore(string s)
     {
-        int n = s.size(), maxx = 0;
-        vector<int> pref(n+1,0), suff(n+1,0);
-        for(int i=0;i<n;i++)
+        ios_base::sync_with_stdio(false);
+        int zero = 0, ones = 0, maxi = INT_MIN;
+        for(int i=0;i<s.size();i++)
         {
-            cout << "Reaching " << i << "\n";
-            pref[i] += (i > 0 ? pref[i-1] : 0) + (s[i]=='0');
-            suff[n-i-1] += (i<n-1 ? suff[n-i] : 0) + (s[n-i-1]=='1');
+            if(s[i]=='0')   zero++;
+            else ones++;
+            if(i != s.size()-1)
+                maxi = max(maxi, zero-ones);
         }
-        for(int i=0; i<n-1;i++)
-        {
-            maxx = max(pref[i]+suff[i+1], maxx);
-        }
-        return maxx;
+        return maxi+ones;
+        
     }
 };
