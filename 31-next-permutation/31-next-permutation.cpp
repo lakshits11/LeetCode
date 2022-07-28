37683882a95f4ae007cbc2e37b9378605e689645
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-28 23:04:05 +0530

    6 ms (69%)

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
-        ios_base::sync_with_stdio(false);
-        int n = arr.size();
-        if(n==0 || n==1) return;
-        int i = n - 2;
-        while(i>=0 && arr[i] >= arr[i+1])  i--;
-        if(i>=0)
+    void nextPermutation(vector<int>& nums)
+    {
+        int n = nums.size(), k, l;
+        
+        // finding a break point
+        for(k = n-2; k >= 0; k--)
         {
-            int j = n-1;
-            while(arr[j]<=arr[i])   j--;
-            swap(arr[j], arr[i]);
+            if(nums[k]<nums[k+1])
+                break;
         }
-        reverse(arr.begin()+i+1, arr.end());
+
+        // if we didnt found any break point, it means we are at last permutation
+        // and we have ti return the first (starting) permutation, and that will be sorted value of array, (in present, we have reverse sorted array)
+        if(k<0)
+            reverse(nums.begin(), nums.end());
+
+        else
+        {
+            // else again start traversing from back and find the
+            // first index whose value is greater than nums[k]
+            for(l = n-1; l > k; l--)
+            {
+                if(nums[l] > nums[k])
+                    break;
+            }
+            swap(nums[k], nums[l]);
+            reverse(nums.begin()+k+1, nums.end());
+        }        
+        
     }
 };
