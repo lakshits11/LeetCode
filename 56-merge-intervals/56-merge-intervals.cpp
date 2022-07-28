d1b76ef862c75e2431f159abd757fde908d1af19
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-07-28 21:38:15 +0530

    Time: 84 ms (20.35%), Space: 20.1 MB (26.05%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intr)
+    {
+        sort(intr.begin(), intr.end());
+        vector<vector<int>> ans;
+        for(vector<int> i : intr)
+        {
+            if(ans.empty() || ans.back()[1] < i[0])
+                ans.push_back(i);
+            else
+                ans.back()[1] = max(ans.back()[1], i[1]);
+        }
+        return ans;
+    }
+};
