9dd8b336ee289322e1ba63cce05cb51023963b60
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-04 19:52:27 +0530

    Time: 2 ms (65.49%), Space: 6.2 MB (57.97%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -7,6 +7,8 @@ public:
             if(number[i]==digit && number[i+1]>digit)
                 return number.substr(0,i)+number.substr(i+1);
         }
+        
+        // if above conditions do not occur, then find the last occurence of number.
         int lst = number.rfind(digit);
         return number.substr(0,lst)+number.substr(lst+1);
     }
