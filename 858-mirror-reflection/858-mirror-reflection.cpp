e3c13ea20576f8043044f4286b612eb218e4a5ed
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-04 10:49:01 +0530

    Time: 4 ms (23.97%), Space: 5.9 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,14 @@
+// By Lakshit Somani
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int mirrorReflection(int p, int q)
+    {
+        while (p % 2 == 0 && q % 2 == 0)
+            p >>= 1, q >>= 1;
+        return 1 - p % 2 + q % 2;
+    }
+};
