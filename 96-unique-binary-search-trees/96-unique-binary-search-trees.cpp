0a7b434ca9e242e374b22d3889db7074c567b550
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-11 17:34:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (65.59%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int numTrees(int n) {
+    int numTrees(int n)
+    {
         int dp[n+1];
         memset(dp, 0, sizeof(dp));
-        dp[0]=1;
-        dp[1]=1;
+        dp[0] = 1; // 0 nodes => empty tree => total 1 tree
+        dp[1] = 1;
         
-        for(int i=2;i<=n;i++)
+        for(int i = 2; i <= n; i++)
         {
             for(int j=1;j<=i;j++)
             {
-                dp[i] += dp[j-1]*dp[i-j];
+                dp[i] += dp[j-1] * dp[i-j];
             }
         }
-        
         return dp[n];
+        
+        
     }
 };
