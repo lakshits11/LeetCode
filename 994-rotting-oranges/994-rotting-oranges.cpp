30feb6448476ea986921a26f3fa1dafcd0a6a8b6
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-13 17:07:28 +0530

    Time: 3 ms (98.17%), Space: 13.2 MB (46.88%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
+    int orangesRotting(vector<vector<int>>& grid)
+    {
         queue<pair<int, int>> q;
-        int days = 0, total = 0, count = 0;
-        for(int i=0;i<grid.size();i++)
+        // count is count of rotten oranges after doing bfs
+        int total = 0, count = 0, days = 0;
+        
+        for(int i = 0; i < grid.size(); i++)
         {
-            for(int j=0;j<grid[0].size();j++)
+            for(int j = 0; j < grid[0].size(); j++)
             {
-                if(grid[i][j]!=0)
+                if(grid[i][j] == 2)
+                    q.emplace(i, j);
+                if(grid[i][j] != 0)
                     total++;
-                if(grid[i][j]==2)
-                    q.push({i,j});
             }
         }
         
-        int dir[5] = {0, -1, 0, 1, 0};
+        const int dir[5] = {-1, 0, 1, 0, -1};
+        
         while(!q.empty())
         {
             int qlen = q.size();
             count += qlen;
-            while(qlen--)
+            for(int i = 0; i < qlen; i++)
             {
-                pair<int, int> curr = q.front();
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
-                for(int i=0; i<4; i++)
+                for(int k = 0; k < 4; k++)
                 {
-                    int nx = curr.first + dir[i];
-                    int ny = curr.second + dir[i+1];
-                    if(nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[nx][ny] == 1)
+                    int nx = x + dir[k];
+                    int ny = y + dir[k+1];
+                    if(nx >= 0 && ny >= 0 && nx < grid.size() && ny < grid[0].size() && grid[nx][ny] == 1)
                     {
                         q.push({nx, ny});
                         grid[nx][ny] = 2;
@@ -36,7 +41,12 @@ public:
             }
             if(!q.empty())
                 days++;
-        }
-        return (total==count ? days : -1);
+        }        
+        // now finally we have to check if all the fresh oranges have converted into rotten or not.
+        // that why we do total == count
+        // if we dont do: we fail at this test case:
+        // grid = [[2,1,1],[0,1,1],[1,0,1]]
+        // bottom left corner orange never gets rotten since it is not in contact with any of the fresh orange.
+        return (total == count) ? days : -1;
     }
 };
