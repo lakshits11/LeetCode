ddb3419bae884fd16eb4a405cd74c9c2224d2593
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-24 20:29:05 +0530

    Time: 65 ms (94.82%), Space: 19.3 MB (74.38%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,55 @@
+// By Lakshit Somani
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>> &grid)
+    {
+        // edge case: start or end not accessible
+        if (grid[0][0] || grid.back().back())
+            return -1;
+        int dir[5] = {-1, 0, 1, 0, -1};
+        int diag[5] = {1, -1, -1, 1, 1};
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        grid[0][0] = 1;
+        int dist = 1;
+
+        while (!q.empty())
+        {
+            int qlen = q.size();
+            while (qlen--)
+            {
+                pair<int, int> node = q.front();
+                q.pop();
+                int x = node.first, y = node.second;
+                if (x == m - 1 && y == n - 1)
+                    return dist;
+                for (int k = 0; k < 4; k++)
+                {
+                    int nx = x + dir[k], ny = y + dir[k + 1];
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] != 1)
+                    {
+                        q.push({nx, ny});
+                        grid[nx][ny] = 1;
+                    }
+                }
+                for (int k = 0; k < 4; k++)
+                {
+                    int nx = x + diag[k], ny = y + diag[k + 1];
+                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] != 1)
+                    {
+                        q.push({nx, ny});
+                        grid[nx][ny] = 1;
+                    }
+                }
+            }
+            dist++;
+        }
+
+        return -1;
+    }
+};
