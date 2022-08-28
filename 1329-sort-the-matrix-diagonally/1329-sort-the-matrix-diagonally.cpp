34d58b81497290304f2fa72d5944ea1811aeeec7
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-28 22:02:04 +0530

    Time: 15 ms (69.45%), Space: 9.4 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,26 +1,35 @@
-class Solution {
-public:
-    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        vector<int> k;
+// By Lakshit Somani
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    void sortAt(int x, int y, vector<vector<int>> &mat)
+    {
         int m = mat.size(), n = mat[0].size();
-        for(int r = 0; r < m ; r++)
+        vector<int> counts(101, 0);
+        for (int i = x, j = y; i < m && j < n; i++, j++)
+            counts[mat[i][j]]++;
+        for (int i = 0; i < 101; i++)
         {
-            k.clear();
-            for(int j = 0, i = r; j < n && i < m; j++, i++)
-                k.push_back(mat[i][j]);
-            sort(k.begin(), k.end());
-            for(int j = 0, i = r; j < n && i < m; j++, i++)
-                mat[i][j] = k[j];
+            while (counts[i])
+            {
+                mat[x++][y++] = i;
+                counts[i]--;
+            }
         }
-        for(int c = 1; c < n; c++)
-        {
-            k.clear();
-            for(int i = 0, j = c; j < n && i < m; i++, j++)
-                k.push_back(mat[i][j]);
-            sort(k.begin(), k.end());
-            for(int i = 0, j = c; j < n && i < m; i++, j++)
-                mat[i][j] = k[i];
-        }
+    }
+
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>> &mat)
+    {
+        int m = mat.size(), n = mat[0].size();
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (i == 0 || j == 0)
+                    sortAt(i, j, mat);
+        
         return mat;
     }
 };
