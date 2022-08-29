d958f7ff2140d004c01c9629342f4da487e23f83
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-29 21:43:25 +0530

    Time: 7 ms (96.71%), Space: 21.3 MB (25.19%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,43 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
+
+-1 => you need a camera
+0  => you have a camera
+1  => you are covered
+
+*/
+
 class Solution {
-public:
-    int sum = 0;
+private:
+    int camera = 0;
     
-    /*
-    Conditions:
-    0 => Node not monitored
-    1 => Node monitored but dont have camera on itself
-    2 => Node has camera on itself
-    */
-    
-    int dfs(TreeNode* node)
+    int f(TreeNode* root)
     {
-        if(node==NULL)  return 1;
-        int left = dfs(node->left), right = dfs(node->right);
+        if(root==NULL)
+            return 1;
+        int lchild = f(root->left);
+        int rchild = f(root->right);
         
-        // If atleast one child is not monitored, we need to place a camera there
-        if(left==0 || right==0){
-            sum++;
-            return 2;
+        if(lchild == -1 || rchild == -1)
+        {
+            camera++;
+            return 0;
         }
         
-        else if(left==2 || right==2)    return 1;
-        return 0;
+        if(lchild == 0 || rchild == 0)
+            return 1;
+        
+        return -1;
     }
     
+public:
     int minCameraCover(TreeNode* root) {
-        // If root isn't monitored, we place an additional camera there
-        if(dfs(root)==0)   sum++;
-        return sum;
+        if(f(root) == -1)
+            camera++;
+        return camera;
     }
 };
