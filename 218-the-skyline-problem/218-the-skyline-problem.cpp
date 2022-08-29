4a158826169cc6aa95f32a4ce1abf5012d5b25b6
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-29 20:07:56 +0530

    Time: 39 ms (82.04%), Space: 14.6 MB (75.22%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.cpp
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -1,30 +1,27 @@
-// By Lakshit Somani
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> getSkyline(vector<vector<int>> &buildings)
-    {
+    vector<vector<int>> getSkyline(vector<vector<int>>& b) {
         multiset<int> st;
         vector<pair<int, int>> vec;
-        vector<vector<int>> res;
-        for (int i = 0; i < buildings.size(); ++i)
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i < b.size(); i++)
         {
-            vec.push_back({buildings[i][0], buildings[i][2]});
-            vec.push_back({buildings[i][1], -buildings[i][2]});
+            vec.push_back({b[i][0], b[i][2]});
+            vec.push_back({b[i][1], -b[i][2]});
         }
+        
         sort(vec.begin(), vec.end());
         int i = 0;
         st.insert(0);
-        int maxe = 0;
-        while (i < vec.size())
+        int maxi = 0;
+        
+        while(i < vec.size())
         {
             int start = vec[i].first;
-            while (i < vec.size() && vec[i].first == start)
+            while(i < vec.size() && vec[i].first == start)
             {
-                if (vec[i].second > 0)
+                if(vec[i].second > 0)
                     st.insert(vec[i].second);
                 else
                 {
@@ -33,12 +30,12 @@ public:
                 }
                 i++;
             }
-            if (*st.rbegin() != maxe)
+            if(*st.rbegin() != maxi)
             {
-                maxe = *st.rbegin();
-                res.push_back({start, maxe});
+                maxi = *st.rbegin();
+                ans.push_back({start, maxi});
             }
         }
-        return res;
+        return ans;
     }
 };
