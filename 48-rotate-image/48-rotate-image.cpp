b216a84d8003a73b8969ff64af8a625078ddb2ab
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-08-30 12:07:39 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for(int i=0;i<matrix.size();i++)
+        
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<=i;j++)
             {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i=0;i<matrix.size();i++)
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<matrix.size()/2;j++)
+            for(int j=0;j<n/2;j++)
             {
                 swap(matrix[i][j], matrix[i][n-j-1]);
             }
-            // cout<<"\n";
         }
+        
     }
 };
