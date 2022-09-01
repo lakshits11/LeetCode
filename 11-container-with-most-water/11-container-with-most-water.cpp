f82f9839dc77b43473e957d20b7734f601d21fd8
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-09-01 14:49:38 +0530

    Time: 93 ms (96.45%), Space: 59.6 MB (36.84%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,22 +1,19 @@
-class Solution
-{
+class Solution {
 public:
-    int maxArea(vector<int> &ht)
+    int maxArea(vector<int>& ht)
     {
         ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
+        int i = 0, j = ht.size()-1, h;
         int water = 0;
-        int i = 0, j = ht.size() - 1, h;
-        while (i < j)
+        while(i < j)
         {
             h = min(ht[i], ht[j]);
-            water = max(water, (j - i) * h);
-            while (ht[i] <= h && i < j)
+            water = max(water, (j-i) * h);
+            while(i<j && ht[i]<=h)
                 i++;
-            while (ht[j] <= h && i < j)
+            while(j>i && ht[j]<=h)
                 j--;
         }
-
         return water;
     }
 };
