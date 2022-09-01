88a9731f62c1fdf501f3194ba09900a18f57c078
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-09-01 10:18:47 +0530

    Time: 105 ms (60.49%), Space: 47.2 MB (14.40%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int superEggDrop(int e, int f) {
+    int superEggDrop(int e, int f)
+    {
         vector<vector<int>> dp(f+1, vector<int>(e+1, 0));
-        int m = 0; // moves
+        int m = 0;
         
-        // dp[M][E] means that, given E eggs and M moves, what is the maximum number of floor that we can check.
         while(dp[m][e] < f)
         {
             m++;
             for(int k = 1; k <= e; k++)
+            {
                 dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1;
+            }
         }
         return m;
+        
     }
 };
