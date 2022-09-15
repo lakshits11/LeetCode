ae91e5bc2470b7fc566c7f9b4a138bcb1df2167a
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-09-15 22:50:46 +0530

    Time: 266 ms (79.16%), Space: 105 MB (65.84%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,36 +1,37 @@
-class Solution {
+class Solution
+{
 private:
     int maxLength = INT_MIN;
-    void dfs(int node, vector<int> &dist, vector<bool> &vis, vector<bool> &extra, vector<int> &edges, int distance)
+    void dfs(int node, vector<int> &dist, vector<int> &vis, vector<int> &dfsVis, vector<int> &adj, int l)
     {
-        if(node != -1)
+        if (node != -1)
         {
-            if(!vis[node])
+            if (!vis[node])
             {
-                vis[node] = true;
-                extra[node] = true;
-                dist[node] = distance;
-                dfs(edges[node], dist, vis, extra, edges, distance + 1);
-                extra[node] = false;
+                vis[node] = 1;
+                dfsVis[node] = 1;
+                dist[node] = l;
+                dfs(adj[node], dist, vis, dfsVis, adj, l + 1);
+                dfsVis[node] = 0;
             }
-            else if(extra[node])
-            {
-                maxLength = max(maxLength, distance-dist[node]);
-            }
+            else if (dfsVis[node])
+                maxLength = max(maxLength, l - dist[node]);
         }
     }
+
 public:
-    int longestCycle(vector<int>& edges)
+    int longestCycle(vector<int> &adj)
     {
-        int n = edges.size();
-        vector<bool> vis(n, false);
-        vector<bool> extra(n, false);
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);
+        int n = adj.size();
+        vector<int> vis(n, 0);
+        vector<int> dfsVis(n, 0);
         vector<int> dist(n, 0);
-        for(int i = 0; i < edges.size(); i++)
+        for (int i = 0; i < n; i++)
         {
-            if(!vis[i])
-                dfs(i, dist, vis, extra, edges, 0);
+            if (!vis[i])
+                dfs(i, dist, vis, dfsVis, adj, 0);
         }
-        return (maxLength==INT_MIN) ? -1 : maxLength;
+        return (maxLength == INT_MIN ? -1 : maxLength);
     }
 };
