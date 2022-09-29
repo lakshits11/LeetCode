02ce6238a5a79d53847a9ba8c8a45ebc2a9d5cf2
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2022-09-29 16:26:19 +0000

    Time: 39 ms (25.22%), Space: 17 MB (91.73%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& time) {
+        arr.push_back({time[0], time[1]});
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        ans.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i][0]<=ans.back().back())
+                ans.back().back() = max(ans.back().back(), arr[i][1]);
+            else ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
