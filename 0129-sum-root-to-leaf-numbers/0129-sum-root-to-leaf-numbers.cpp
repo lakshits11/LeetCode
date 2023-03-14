9d3bd686c8eaeaa57ce5a9ba823d1710a8a0c65b
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-03-14 07:37:37 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (18.96%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        stack<pair<TreeNode*, int>> s;      // {node, cur}
+        s.push({root, 0});
+        int sum = 0;
+        while(!s.empty()) {
+            auto [root, cur] = s.top(); s.pop();
+            cur = cur * 10 + root -> val;
+            if(!root -> left && !root -> right) sum += cur;  // reached leaf node
+            if(root -> right) s.push({root -> right, cur});
+            if(root -> left) s.push({root -> left, cur});
+        }
+        return sum;
+    }
+};
