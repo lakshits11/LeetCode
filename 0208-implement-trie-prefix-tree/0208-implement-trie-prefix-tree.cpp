d60f926be88508884544306a91c2eed679d44705
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-10-24 10:33:22 +0530

    Time: 43 ms (86.88%), Space: 44.1 MB (31.78%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,59 +1,58 @@
 class TrieNode
 {
 public:
-    TrieNode *next[26];
-    bool is_word;
+    TrieNode* next[26];
+    bool isWord;
     
-    // Initialize your data structure here.
     TrieNode(bool b = false)
     {
         memset(next, 0, sizeof(next));
-        is_word = b;
+        isWord = b;
     }
 };
 
-class Trie
-{
-    TrieNode *root;
+
+class Trie {
 public:
-    Trie()
-    {
+    TrieNode* root;
+    Trie() {
         root = new TrieNode();
     }
-
-    // Inserts a word into the trie.
-    void insert(string s)
-    {
-        TrieNode *p = root;
-        for(int i = 0; i < s.size(); ++ i)
+    
+    void insert(string s) {
+        TrieNode* p = root;
+        for(int i=0;i<s.size();i++)
         {
-            if(p -> next[s[i] - 'a'] == NULL)
-                p -> next[s[i] - 'a'] = new TrieNode();
-            p = p -> next[s[i] - 'a'];
+            if(p->next[s[i]-'a'] == NULL)
+                p->next[s[i]-'a'] = new TrieNode();
+            p = p->next[s[i]-'a'];
         }
-        p -> is_word = true;
+        p->isWord = true;
     }
-
-    // Returns if the word is in the trie.
-    bool search(string key)
-    {
-        TrieNode *p = find(key);
-        return p != NULL && p -> is_word;
+    
+    bool search(string s) {
+        TrieNode* p = root;
+        for(int i = 0; i < s.size() && p != NULL; ++i)
+        {
+            p = p->next[s[i]-'a'];
+        }
+        return p!=NULL && p->isWord;
     }
-
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    bool startsWith(string prefix)
-    {
-        return find(prefix) != NULL;
-    }
-
-private:
-    TrieNode* find(string key)
-    {
-        TrieNode *p = root;
-        for(int i = 0; i < key.size() && p != NULL; ++ i)
-            p = p -> next[key[i] - 'a'];
-        return p;
+    
+    bool startsWith(string s) {
+        TrieNode* p = root;
+        for(int i = 0; i < s.size() && p != NULL; ++i)
+        {
+            p = p->next[s[i]-'a'];
+        }
+        return p!=NULL;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
