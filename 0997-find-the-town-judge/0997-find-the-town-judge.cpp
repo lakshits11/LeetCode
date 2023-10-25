97df2f24e2e760c0b9b18ca5e9a1a9a2e383ede2
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-10-25 10:33:42 +0530

    Time: 119 ms (97.19%), Space: 60.9 MB (84.16%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
-        int indegree[n+1];
-        int outdegree[n+1];
-        
-        for (int i = 0; i < n + 1; i++)
+        int ind[n+1];
+        int outd[n+1];
+        for(int i=0;i<n+1;i++)
         {
-            indegree[i] = 0;
-            outdegree[i] = 0;
+            ind[i]=0;outd[i]=0;
         }
-        
-        for(int i=0;i<trust.size();i++)
-        {
-            outdegree[trust[i][0]]++;
-            indegree[trust[i][1]]++;
+        for(int i=0;i<trust.size();++i){
+            outd[trust[i][0]]++;
+            ind[trust[i][1]]++;
         }
-        
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<n+1;i++)
         {
-            if(outdegree[i]==0 && indegree[i]==n-1)
+            if(outd[i]==0 && ind[i]==n-1)
                 return i;
         }
-        
         return -1;
     }
 };
