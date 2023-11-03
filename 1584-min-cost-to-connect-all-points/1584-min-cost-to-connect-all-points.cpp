e1f614627cce4715ae569b598eaac24f0bd1d072
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-11-03 15:16:56 +0530

    Time: 522 ms (37.98%), Space: 58.6 MB (41.09%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,80 +1,74 @@
-class UnionFind {
-public:
-    vector<int> group;
-    vector<int> rank;
-
-    UnionFind(int size) {
-        group = vector<int>(size);
-        rank = vector<int>(size);
-        for (int i = 0; i < size; ++i) {
-            group[i] = i;
-        }
-    }
-
-    int find(int node) {
-        if (group[node] != node) {
-            group[node] = find(group[node]);
-        }
-        return group[node];
-    }
-
-    bool join(int node1, int node2) {
-        int group1 = find(node1);
-        int group2 = find(node2);
-        
-        // node1 and node2 already belong to same group.
-        if (group1 == group2) {
-            return false;
-        }
-
-        if (rank[group1] > rank[group2]) {
-            group[group2] = group1;
-        } else if (rank[group1] < rank[group2]) {
-            group[group1] = group2;
-        } else {
-            group[group1] = group2;
-            rank[group2] += 1;
-        }
-
-        return true;
-    }
-};
-
-
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rankk;
+    
+    struct node
+    {
+        int u, v, wt;
+        node(int wgt, int i, int j)
+        {
+            wt=wgt;
+            u=i;
+            v=j;
+        }
+    };
+    
+    int findp(int u)
+    {
+        if(u==parent[u]) return u;
+        return parent[u] = findp(parent[u]);
+    }
+    
+    void uni(int u, int v)
+    {
+        u = findp(u);
+        v = findp(v);
+        if(rankk[u]<rankk[v]) swap(u,v);
+        parent[v]=u;
+        if(rankk[u]==rankk[v]) rankk[u]++;
+    }
+    
     int minCostConnectPoints(vector<vector<int>>& points) {
+        // APPROACH THINKING
+        // firstly connect each pair of points wiht a weighted edge and
+        // then find mst weight amongst them. that will be minimum cost to
+        // connect all paths
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int n = points.size();
-        vector<pair<int, pair<int, int>>> allEdges;
-        
-        // Storing all edges of our complete graph.
-        for (int currNode = 0; currNode < n; ++currNode) {
-            for (int nextNode = currNode + 1; nextNode < n; ++nextNode) {
-                int weight = abs(points[currNode][0] - points[nextNode][0]) + 
-                             abs(points[currNode][1] - points[nextNode][1]);
-                
-                allEdges.push_back({ weight, { currNode, nextNode }});
+        // weight and point coordinates
+        vector<node> adj;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                // connects points[i] to points[j]
+                // not vice cersa since we will be using DSU to find MST
+                int wt = abs(points[j][0]-points[i][0]) + abs(points[j][1] - points[i][1]);
+                adj.push_back(node(wt, i, j));
             }
         }
         
-        // Sort all edges in increasing order.
-        sort(allEdges.begin(), allEdges.end());
+        // now we have a mst built with node distances as weights and node values as 0, 1, 2...n-1
+        parent.resize(n);
+        rankk.resize(n, 1);
+        for(int i=0;i<n;i++)
+            parent[i]=i;
         
-        UnionFind uf(n);
-        int mstCost = 0;
-        int edgesUsed = 0;
-        
-        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i) {
-            int node1 = allEdges[i].second.first;
-            int node2 = allEdges[i].second.second;
-            int weight = allEdges[i].first;
-            
-            if (uf.join(node1, node2)) {
-                mstCost += weight;
-                edgesUsed++;
+        int cost = 0;
+        sort(adj.begin(), adj.end(), [](const node& a, const node& b) {
+            return a.wt < b.wt;
+        });
+        for(auto it : adj)
+        {
+            if(findp(it.u) != findp(it.v))
+            {
+                cost += it.wt;
+                uni(it.u, it.v);
             }
         }
+        return cost;
         
-        return mstCost;
     }
 };
