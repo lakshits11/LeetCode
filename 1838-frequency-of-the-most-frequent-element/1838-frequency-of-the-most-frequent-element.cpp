ff4207fa8ba2cde74c2dbe4a5cd06bceb4c627ec
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-11-18 12:45:06 +0530

    Time: 195 ms (13.22%), Space: 99.5 MB (13.51%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k)
-    {
-        // condition to check always:
-        // expand window while nums[r] * windowLenght <= total + k
-        // but this algo only works if arr is sorted
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        sort(nums.begin(), nums.end());
-        long long l = 0, r = 0;
-        long long res = 0, total = 0;
-        while(r < nums.size())
-        {
-            total += nums[r];
-            while(nums[r]*(r-l+1) > total + k)
-            {
-                total -= nums[l];
-                l++;
-            }
-            res = max(res, r - l + 1);
-            r++;
-        }
-        return res;
-        
+    int maxFrequency(vector<int>& nums, int k) {
+    int res = 1;
+    sort(begin(nums), end(nums));
+    for (int i = nums.size() - 1, j = i; i > 0; --i) {
+        for (; j >= 0 && k >= nums[i] - nums[j]; --j)
+            k -= nums[i] - nums[j];
+        res = max(res, i - j);
+        k += (nums[i] - nums[i - 1]) * (i - j - 1);
     }
+    return res;
+}
 };
