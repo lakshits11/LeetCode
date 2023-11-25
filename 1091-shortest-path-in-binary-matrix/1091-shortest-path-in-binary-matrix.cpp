ae96693b95bb9371e0d0b35623c13aecd5f21182
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-11-25 16:17:25 +0530

    Time: 35 ms (99.51%), Space: 20 MB (87.14%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,39 @@
-// By Lakshit Somani
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>> &grid)
-    {
-        // edge case: start or end not accessible
-        if (grid[0][0] || grid.back().back())
-            return -1;
-        int dir[5] = {-1, 0, 1, 0, -1};
-        int diag[5] = {1, -1, -1, 1, 1};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if(grid[0][0] || grid.back().back()) return -1;
+        int dir[5] = {-1,0,1,0,-1};
+        int diag[5] = {1, -1,-1,1,1};
+        
         int m = grid.size(), n = grid[0].size();
         queue<pair<int, int>> q;
-        q.push({0, 0});
+        q.push({0,0});
         grid[0][0] = 1;
         int dist = 1;
-
-        while (!q.empty())
+        while(!q.empty())
         {
             int qlen = q.size();
-            while (qlen--)
+            while(qlen--)
             {
                 pair<int, int> node = q.front();
                 q.pop();
                 int x = node.first, y = node.second;
-                if (x == m - 1 && y == n - 1)
-                    return dist;
-                for (int k = 0; k < 4; k++)
+                if(x==m-1 && y==n-1) return dist;
+                for(int k = 0; k < 4; k++)
                 {
-                    int nx = x + dir[k], ny = y + dir[k + 1];
-                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] != 1)
+                    int nx = x+dir[k], ny = y+dir[k+1];
+                    if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny] != 1)
                     {
                         q.push({nx, ny});
                         grid[nx][ny] = 1;
                     }
                 }
-                for (int k = 0; k < 4; k++)
+                for(int k = 0; k < 4; k++)
                 {
-                    int nx = x + diag[k], ny = y + diag[k + 1];
-                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] != 1)
+                    int nx = x+diag[k], ny = y+diag[k+1];
+                    if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny] != 1)
                     {
                         q.push({nx, ny});
                         grid[nx][ny] = 1;
@@ -49,7 +42,6 @@ public:
             }
             dist++;
         }
-
         return -1;
     }
 };
