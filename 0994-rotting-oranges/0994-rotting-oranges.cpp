5150e548ac94ef3e7a6453c21bed967d8caa0b36
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-05 13:00:15 +0530

    Time: 4 ms (65.16%), Space: 13.4 MB (68.17%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);
+        queue<pair<int, int>> q;
+        int days = 0, total = 0, count = 0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]!=0)
+                    total++;
+                if(grid[i][j]==2)
+                    q.push({i,j});
+            }
+        }
+        
+        int dir[5] = {0, -1, 0, 1, 0};
+        while(!q.empty())
+        {
+            int qlen = q.size();
+            count += qlen;
+            while(qlen--)
+            {
+                pair<int, int> curr = q.front();
+                q.pop();
+                for(int i=0; i<4; i++)
+                {
+                    int nx = curr.first + dir[i];
+                    int ny = curr.second + dir[i+1];
+                    if(nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[nx][ny] == 1)
+                    {
+                        q.push({nx, ny});
+                        grid[nx][ny] = 2;
+                    }
+                }
+            }
+            if(!q.empty())
+                days++;
+        }
+        return (total==count ? days : -1);
+    }
+};
