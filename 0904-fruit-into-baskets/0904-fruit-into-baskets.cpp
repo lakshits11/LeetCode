c3cd8756f224507d0b23bb6186d2ce2b971db612
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-07 17:50:11 +0530

    Time: 99 ms (86.80%), Space: 71.6 MB (48.55%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,15 +1,27 @@
-class Solution {
+/*
+This is essentially the same as Longest substring with at most K distinct characters, but it is subarrays with at most 2 distinct numbers.
+*/
+
+class Solution
+{
 public:
-    int totalFruit(vector<int> &tree) {
-        unordered_map<int, int> count;
-        int i, j;
-        for (i = 0, j = 0; j < tree.size(); ++j) {
-            count[tree[j]]++;
-            if (count.size() > 2) {
-                if (--count[tree[i]] == 0)count.erase(tree[i]);
-                i++;
+    int totalFruit(vector<int> &f)
+    {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);
+        int r = 0, l = 0;
+        int len = -1;
+        unordered_map<int, int> u;
+        for (r = 0; r < f.size(); r++)
+        {
+            u[f[r]]++;
+            while (u.size() > 2)
+            {
+                u[f[l]]--;
+                if (u[f[l]] == 0) u.erase(f[l]);
+                l++;
             }
+            len = max(len, r - l + 1);
         }
-        return j - i;
+        return len;
     }
 };
