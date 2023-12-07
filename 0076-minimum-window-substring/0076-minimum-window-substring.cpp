5da87b10754209c3becf653d7c1d66340fbc180f
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-07 17:18:43 +0530

    Time: 6 ms (92.55%), Space: 8.4 MB (34.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,16 +1,40 @@
-class Solution {
+static const auto speedup = []() -> int
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution
+{
 public:
-    string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter = t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+    string minWindow(string s, string t)
+    {
+        unordered_map<char, int> count;
+        int minJ = INT_MAX, minI = 1, i = 0;
+        for (int i = 0; i < t.size(); ++i)
+        {
+            count[t[i]]++;
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        // count of remaining chars needed to be present in string s
+        int remaining = count.size();
+        for (int j = 0; j < s.size(); ++j)
+        {
+            count[s[j]]--;
+
+            if (count[s[j]] == 0) remaining--; // that means the count of s[j] in current window has equaled to count of s[j] in t. So we have one less character to process. Hence remaining is decreased by one.
+            while (remaining == 0)
+            {
+                if (j - i < minJ - minI)
+                {
+                    minJ = j, minI = i;
+                }
+                // remove i from window and increment i
+                count[s[i]]++;
+                if (count[s[i]] > 0) remaining++;
+                i++;
+            }
+        }
+        return minJ == INT_MAX ? "" : s.substr(minI, minJ - minI + 1);
     }
 };
