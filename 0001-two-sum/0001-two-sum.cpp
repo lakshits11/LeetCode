6864604c066ad04e11aae49f47e41fa8560161f8
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-07 10:43:28 +0530

    Time: 7 ms (89.51%), Space: 11.3 MB (25.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
