9430cd33ed80c7fded8e8b0a85dfe8d9506dc3ad
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-14 00:05:17 +0530

    Time: 27 ms (100.00%), Space: 53.2 MB (21.42%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,35 +1,38 @@
- /*
-Approach : 
-Keep on checking if the complimetary value is already seen or not, if seen then simply
-reduce the count of the complimentary one and calculate the result. 
-
-To check if all the elements are metched or not a count can be used to see it is equal to zero or not.
-count can be increament when a match found and decremented when not found.
-*/
-
-static const auto speedup = []() -> int{
-    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+static const auto speedup = []() -> int
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
 }();
 
-class Solution {
+class Solution
+{
 public:
-long long dividePlayers(vector<int>& skill) {
-    long long ans = 0;
-    unordered_map<int, int> mp;
-    long sum = 0, count = 0, n = skill.size()/2;
-    for(auto s: skill) sum+= s; 
-    if(sum % n != 0) return -1;
-    int teamSize = sum/n;
-    for(auto s: skill){ 
-        if(mp[teamSize - s] > 0 ){
-            mp[teamSize - s]--; // since we have already considered current element, so we need not consider its complimentary element, mthats why we did reduce count of its complimentary element
-            ans += s * (teamSize - s);
-            count--;
-        }else{
-            mp[s]++; count++;
+    long long dividePlayers(vector<int> &skill)
+    {
+        long long ans = 0;
+        int mp[100005] = {0};
+        long sum = 0, count = 0, n = skill.size() / 2;
+        for (int &s : skill)
+            sum += s;
+        if (sum % n != 0) return -1;
+        int teamSize = sum / n;
+        for (int &s : skill)
+        {
+            if (mp[teamSize - s] > 0)
+            {
+                // since we have already considered current element, so we need not consider its complimentary element, mthats why we did reduce count of its complimentary element
+                mp[teamSize - s]--;
+                ans += s * (teamSize - s);
+                count--;
+            }
+            else
+            {
+                mp[s]++;
+                count++;
+            }
         }
+        if (count != 0) return -1;
+        return ans;
     }
-    if(count != 0 ) return -1;
-    return ans;
-}
 };
