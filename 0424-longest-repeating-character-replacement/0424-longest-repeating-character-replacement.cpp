665edb00f93bc7d56dd189148473c4388ff7a41d
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-13 09:28:55 +0530

    Time: 3 ms (98.09%), Space: 7.3 MB (92.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,40 @@
+/*
+In case anyone is confused by this solution, here's another way of explaining it:
+
+end-start+1 = size of the current window
+maxCount = largest count of a single, unique character in the current window
+
+The main equation is: end-start+1-maxCount
+
+When end-start+1-maxCount == 0, then then the window is filled with only one character
+When end-start+1-maxCount > 0, then we have characters in the window that are NOT the character that occurs the most. end-start+1-maxCount is equal to exactly the # of characters that are NOT the character that occurs the most in that window. Example: For a window "xxxyz", end-start+1-maxCount would equal 2. (maxCount is 3 and there are 2 characters here, "y" and "z" that are not "x" in the window.)
+
+We are allowed to have at most k replacements in the window, so when end-start+1-maxCount > k, then there are more characters in the window than we can replace, and we need to shrink the window.
+
+If we have window with "xxxy" and k = 1, that's fine because end-start+1-maxCount = 1, which is not > k. maxLength gets updated to 4.
+
+But if we then find a "z" after, like "xxxyz", then we need to shrink the window because now end-start+1-maxCount = 2, and 2 > 1. The window becomes "xxyz".
+
+maxCount may be invalid at some points, but this doesn't matter, because it was valid earlier in the string, and all that matters is finding the max window that occurred anywhere in the string. Additionally, it will expand if and only if enough repeating characters appear in the window to make it expand. So whenever it expands, it's a valid expansion.
+
+Hope that helps.
+
+P.S. Yes, as several other people mentioned already, the while should be replaced with if. The time complexity is exactly the same, because the while-loop only runs once anyway.
+
+*/
+
+/*
+
+The problem says that we can make at most k changes to the string (any character can be replaced with any other character). So, let's say there were no constraints like the k. Given a string convert it to a string with all same characters with minimal changes. The answer to this is
+
+length of the entire string - number of times of the maximum occurring character in the string
+
+Given this, we can apply the at most k changes constraint and maintain a sliding window such that
+
+(length of substring - number of times of the maximum occurring character in the substring) <= k
+
+*/
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
