8c372f127ff96b5ff8347631d448f11370c944db
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-13 23:12:50 +0530

    Time: 193 ms (81.50%), Space: 235.6 MB (63.00%) - LeetHub

--- 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.cpp
+++ 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.cpp
@@ -1,3 +1,20 @@
+/*
+Let us study the first example given in the problem:
+Input: nums = [0,1,0,0,1]
+Output: 3
+Explanation: There are 3 ways to split nums into good subarrays:
+
+    [0,1] [0,0,1]
+    [0,1,0] [0,1]
+    [0,1,0,0] [1]
+
+    Traverse the input array, before encountering first 1, there is NO good subarray at all. Therefore, we initialize the cnt as 0;
+    After finding the first 1, we at least can have 1 good subarray and hence we change cnt from 0 to 1;
+    Then the sliding window starts: the previous 1 as lower bound, lo, exclusively, next 1 as upper bound, hi, inclusively (we can refer to the above example for more details.); for each sliding window, we multiply its width, hi - lo, to cnt;
+    Repeat step 3 till the end of the input, and the result modulo 1_000_000_007 is the solution.
+
+*/
+
 class Solution {
 public:
     int numberOfGoodSubarraySplits(vector<int>& nums) {
