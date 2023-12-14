1d0c2f2e3f8b02105dc14fd0c846aa92baf1fc73
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-15 00:12:29 +0530

    Time: 251 ms (12.94%), Space: 108 MB (77.06%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -1,10 +1,14 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+unordered_set<int> s;
 class Solution {
 public:
     int shortestSequence(vector<int>& rolls, int k)
     {
-        // ios_base::sync_with_stdio(false);
         int res = 1;
-        set<int> s;
+        s.clear();
         for(int i=0;i<rolls.size();i++)
         {
             s.insert(rolls[i]);
