13531c314033c654b023edf0fe4931ee1404c331
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-15 09:25:53 +0530

    Time: 7 ms (85.29%), Space: 11.4 MB (32.54%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,23 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) 
+    {
+        int m = paths.size();
+        unordered_map<string, int> mp;
+        
+        for(int i = 0; i < m; i++)
+           mp[paths[i][0]]++;
+        
+        for(int i = 0; i < m; i++)
+        {
+            if(mp[paths[i][1]] < 1)
+                return paths[i][1];
+        }
+        
+        return "";   
+    }
+};
