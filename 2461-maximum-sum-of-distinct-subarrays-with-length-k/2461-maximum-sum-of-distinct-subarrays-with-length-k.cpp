e2049da2254da8781fde3c11f78961044a6303f4
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-16 21:38:36 +0530

    Time: 79 ms (99.80%), Space: 146 MB (5.12%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,34 @@
+// #define ll long long
+
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution 
+{
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) 
+    {
+        long long res = 0, cur = 0, dup = -1;
+        vector<long long> pos(100001,-1); // to store last seen position of an element
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            cur += nums[i];
+            if (i >= k) cur -= nums[i-k]; // after this code, our window size = k condition satisfies
+            
+            // now we are only left with no duplicate condition
+            
+            // updating the last seen position of current element
+            dup = max(dup, pos[nums[i]]);
+            
+            // if that position falls out of window, hence we are good to go
+            if (i - dup >= k) res = max(res, cur);
+            
+            // updating current position as last seen position of our current element
+            pos[nums[i]] = i;
+        }
+        
+        return res;
+    }
+};
