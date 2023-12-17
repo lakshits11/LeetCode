1434b0cddddd2d71162fabb3ccf8d9da7ff3e859
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-17 09:10:17 +0530

    Time: 493 ms (26.31%), Space: 167.6 MB (28.71%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,39 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+struct comp{
+    bool operator()(const pair<int, string> &a, const pair<int, string> &b)const
+    {
+        if(a.first == b.first)
+            return a.second < b.second;
+        return a.first > b.first;
+    }
+};
+
+class FoodRatings {
+public:
+    map<string, int> r;
+    map<string, set<pair<int, string>, comp>> m;
+    map<string, string> c;
+    
+    FoodRatings(vector<string>& food, vector<string>& cuisines, vector<int>& ratings) {
+        int n = food.size();
+        for(int i=0;i<n;i++)
+        {
+            r[food[i]] = ratings[i];
+            m[cuisines[i]].insert({ratings[i], food[i]});
+            c[food[i]] = cuisines[i];
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        m[c[food]].erase({r[food], food});
+        m[c[food]].insert({newRating, food});
+        r[food] = newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        return (*m[cuisine].begin()).second;
+    }
+};
