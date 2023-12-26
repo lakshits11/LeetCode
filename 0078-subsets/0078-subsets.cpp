051e9a82aee89b80f075b9617681d2ab5c2edd04
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-26 21:30:36 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (51.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,23 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
 class Solution {
 public:
     vector<vector<int>> ans;
-    void dfs(int index, vector<int> temp, vector<int> &nums)
+    
+    void dfs(int i, vector<int> &temp, vector<int> &nums)
     {
         ans.push_back(temp);
-        for(int j=index; j<nums.size();j++)
+        for(int j = i; j < nums.size(); j++)
         {
             temp.push_back(nums[j]);
             dfs(j+1, temp, nums);
             temp.pop_back();
         }
     }
-        
-    vector<vector<int>> subsets(vector<int>& nums) {
+    
+    vector<vector<int>> subsets(vector<int>& nums){
         vector<int> temp;
         dfs(0, temp, nums);
         return ans;
