5c2de75f9f75dcb3915be80bbf040045e7c5920a
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-28 20:12:06 +0530

    Time: 42 ms (79.78%), Space: 6.5 MB (100.00%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
-    const static int N = 127;
-
-    // dp[left][k] means the minimal coding size for substring 
-    // s[left:] and removing at most k chars
-    int dp[N][N];
-
-    string str;
-    int n;
-
-    // get length of digit
-    inline int xs(int x) { return x == 1 ? 0 : x < 10 ? 1 : x < 100 ? 2 : 3; }
-
-    int solve(int left, int k) {
-        if(k < 0) return N;  // invalid, return INF
-        if(left >= n or n - left <= k) return 0;  // empty
-
-        int& res = dp[left][k];
-        if(res != -1) return res;
-        res = N;
-
-        int cnt[26] = {0};
-        // we try to make s[left:j] (both inculded) as one group,
-        // and all chars in this group should be the same.
-        // so we must keep the most chars in this range and remove others
-        // the range length is (j - left + 1)
-        // and the number of chars we need to remove is (j - left + 1 - most)
-        for(int j = left, most = 0; j < n; j++) {
-            most = max(most, ++cnt[str[j] - 'a']);  // most = max(count(s[left:j])
-            res = min(res, 1 + xs(most) + solve(j + 1, k - (j - left + 1 - most)));
+private:
+    int memo[101][101];
+	
+    int recursion(const string &s, int i, int K){
+        int n = s.length(), k = K;
+        // if # of chars left are < k than all can be removed
+        // resultant length of string at this stage will be 0
+        if(n-i<=k) return 0;
+        if(memo[i][k]!=-1) return memo[i][k];
+		
+		// solving for when ith character is not taken into code
+		// when k is non zero else assigning a large number to answer
+		// c to count the occurence of the ith element
+		// initially set to 1 as the its character is already taken into account
+        int ans = k?recursion(s,i+1,k-1):101, c = 1;
+		
+		// running a loop to group the similar characters
+		// notice running the loop upto n because the previous character
+		// is added to the resultant encoded code's length after its loop is over
+        for(int j=i+1;j<=n;j++){
+			// adding previous character to resultant code's length, adding 1, 2 or 3 to take
+			// account of the count of current character w.r.t. c and sending the rest of the
+			// string for similar evaluation
+            ans = min(ans,1+((c>99)?3:(c>9)?2:(c>1)?1:0)+recursion(s,j,k));
+			
+            if(j<n and s[i]==s[j]) c++;
+			// here using else if because we do not need to break if k is negative
+			// and s[i] == s[j], because they are similar and might not add to the length
+			// of the encoded code length
+            else if(--k<0) break;
         }
-        return res;
+        return memo[i][K] = ans;
     }
+    
 public:
     int getLengthOfOptimalCompression(string s, int k) {
-        memset(dp, -1, sizeof(dp));
-        str = s;
-        n = s.size();
-        return solve(0, k);
+        memset(memo,-1,sizeof(memo));
+        return recursion(s,0,k);
     }
 };
