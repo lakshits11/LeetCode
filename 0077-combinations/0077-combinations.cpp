15802dfd11b341755093d05d8e0b6dc270467c75
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2023-12-28 09:20:32 +0530

    Time: 136 ms (48.36%), Space: 79.8 MB (47.21%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,18 +1,28 @@
+// https://leetcode.com/problems/combinations/discuss/1141903/Clear-and-simple-explanation-with-intuition%3A-100-faster
+
 class Solution {
 public:
-	vector<vector<int>> combine(int n, int k) {
-		vector<vector<int>> result;
-		int i = 0;
-		vector<int> p(k, 0);
-		while (i >= 0) {
-			p[i]++;
-			if (p[i] > n) --i;
-			else if (i == k - 1) result.push_back(p);
-			else {
-			    ++i;
-			    p[i] = p[i - 1];
-			}
-		}
-		return result;
-	}
+    vector<vector<int>> ans;
+    
+    void helper(int idx, int k,vector<int>&current,int n)
+    {
+        if(current.size()==k)    // base case
+        {
+            ans.push_back(current);
+            return;
+        }
+        
+        for(int i=idx;i<n+1;i++)
+        {
+            current.push_back(i);  //consider the current element i
+            helper(i+1,k,current,n); // generate combinations
+            current.pop_back(); //proceed to next element
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>current;
+        helper(1,k,current,n);
+        return ans; //return answer
+    }
 };
