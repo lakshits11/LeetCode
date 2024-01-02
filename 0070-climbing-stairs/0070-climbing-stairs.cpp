e0eaadf5c81e5c7261d1f7a3343324f0b4361602
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-02 17:19:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (70.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,30 @@
+int dp[46];
+
+static const auto speedup = []() -> int {
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        for(int i=4;i<=45;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+    return 0;
+}();
+
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        dp[0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=2;j++){
-                if(j>i)
-                    continue;
-                else{
-                    dp[i]=dp[i]+dp[i-j];
-                }
-            }
-        }
+        // int dp[46];
+        // dp[0]=1;
+        // dp[1]=1;
+        // dp[2]=2;
+        // dp[3]=3;
+        // for(int i=4;i<=n;i++)
+        // {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
         return dp[n];
     }
 };
