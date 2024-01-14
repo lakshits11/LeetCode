7c3885a91ba929fdc1c197354b89c78020c31605
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-14 09:39:53 +0530

    Time: 42 ms (99.15%), Space: 20.9 MB (78.40%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,3 +1,7 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
