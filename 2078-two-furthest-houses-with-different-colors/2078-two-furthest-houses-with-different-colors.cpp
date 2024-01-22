01fd68fb097c0c9a2f2ac7494daaf04eb1781121
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-22 19:41:09 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (5.12%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,13 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    int maxDistance(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (A[0] == A[j]) j--;
+        while (A[n - 1] == A[i]) i++;
+        return max(n - 1 - i, j);
+    }
+};
