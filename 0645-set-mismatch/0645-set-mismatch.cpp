e73ae0f8cfea97b018a4513a69329151863f26f4
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-22 19:25:47 +0530

    Time: 3 ms (100.00%), Space: 23.7 MB (37.21%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,18 +1,25 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int dup = 0, miss = 0;
-        for(int i = 0; i < nums.size(); i++)
+        for(int i = 0; i < nums.size(); ++i)
         {
             int x = abs(nums[i]);
-            if(nums[x-1] < 0 && dup==0)
-               dup = x;
-            else nums[x-1] *= -1;
+            if(nums[x-1]<0 && dup==0)
+                dup=x;
+            else nums[x-1]*=-1;
         }
-        for(int i = 0; i < nums.size(); i++)
+        for(int i=0;i<nums.size();++i)
         {
             if(nums[i]>=0)
-                {miss = i+1;break;}
+            {
+                miss = i+1;
+                break;
+            }
         }
         return {dup, miss};
     }
