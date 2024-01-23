842077fd6e72a9233b1a63f12114716ed48effe4
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-23 21:54:08 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (82.63%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
-    int maxLength(vector<string>& A) {
-        vector<bitset<26>> dp = {bitset<26>()};
-        int res = 0;
-        for (auto& s : A) {
-            bitset<26> a;
-            for (char c : s)
-                a.set(c - 'a');
-            int n = a.count();
-            if (n < s.size()) continue;
-
-            for (int i = dp.size() - 1; i >= 0; --i) {
-                bitset c = dp[i];
-                if ((c & a).any()) continue;
-                dp.push_back(c | a);
-                res = max(res, (int)c.count() + n);
+    int res = 0;
+    void solve(int i, int temp, vector<int> &uniq)
+    {
+        res = max(res, __builtin_popcount(temp));
+        for(int j = i; j < uniq.size(); j++)
+        {
+            // means they have unique chars only
+            if((temp & uniq[j]) == 0)
+            {
+                solve(j+1, temp|uniq[j], uniq);
             }
         }
+        
+        
+    }
+    int maxLength(vector<string>& arr) {
+        vector<int> uniq;
+        for(string &s : arr)
+        {
+            bitset<26> a;
+            for(char c : s)
+                a.set(c-'a');
+            int n = a.count();
+            if(n!=s.size()) continue;
+            
+            int val=0;
+            for(char &ch : s)
+            {
+                val = (val | (1 << (ch - 'a')));
+            }
+            uniq.push_back(val);
+        }
+        int temp = 0;
+        solve(0, temp, uniq);
+        
         return res;
     }
 };
