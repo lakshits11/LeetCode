68b80ee9b71e0127b02c0deae67cd19f0be2c527
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-24 19:03:36 +0530

    Time: 238 ms (94.64%), Space: 178.7 MB (96.25%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,20 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+
+class Solution
+{
 public:
-    int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
+    int pseudoPalindromicPaths(TreeNode *root, int count = 0)
+    {
         if (!root) return 0;
-        count ^= 1 << (root->val - 1);
+        // basically this is toggling the bit present at position root->val.
+        count ^= 1 << (root->val-1);
         int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
+        // At the leaf node, check if the count has only one bit that is 1.
         if (root->left == root->right && (count & (count - 1)) == 0) res++;
         return res;
     }
