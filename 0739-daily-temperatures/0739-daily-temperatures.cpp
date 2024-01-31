61209ab464cd548f13940f8ac6306fb7a94633e9
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-01-31 19:35:32 +0530

    Time: 131 ms (77.16%), Space: 101.1 MB (64.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        stack<int> s;
-        vector<int> nextGreater(n, -1);
-        for(int i=n-1; i>=0; i--) {
-            while(true) {
-                if(s.empty()){
-                    s.push(i);
-                    nextGreater[i] = 0;
-                    break;
-                }else if(temperatures[s.top()] <= temperatures[i]){
-                    s.pop();
-                }else if(temperatures[s.top()] > temperatures[i]){
-                    nextGreater[i] = s.top()-i;
-                    s.push(i);
-                    break;
-                }
-            }
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<pair<int, int>> st;
+        int n = temp.size();
+        vector<int> ans(n, -1);
+        for(int i=n-1;i>=0;--i)
+        {
+            while(!st.empty() && st.top().first<=temp[i])
+                st.pop();
+            if(!st.empty())
+                ans[i] = st.top().second;
+            else ans[i]=-1;
+            
+            st.push({temp[i], i});
         }
-        return nextGreater;
+        for(int i=0;i<n;++i)
+        {
+            if(ans[i]!=-1)
+                ans[i] = ans[i]-i;
+            else ans[i]=0;
+        }
+        return ans;
     }
 };
