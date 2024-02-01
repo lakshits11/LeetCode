12d31421cb1245dc45beee7c71dc16bd2d26b3fe
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-02-01 19:02:47 +0530

    Time: 112 ms (99.91%), Space: 119.2 MB (39.69%) - LeetHub

---
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -0,0 +1,18 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size(); i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return {};
+            }
+            ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+        }
+        return ans;
+    }
+};
