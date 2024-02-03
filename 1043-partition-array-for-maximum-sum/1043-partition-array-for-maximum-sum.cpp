b13b8e1fc71aa56e09dd347435cbe12617203181
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-02-03 19:52:24 +0530

    Time: 6 ms (94.96%), Space: 10.9 MB (23.07%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,20 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& A, int K) {
+        int N = A.size();
+        vector<int> dp(N + 1);
+        for (int i = 1; i <= N; ++i) {
+            int curMax = 0, best = 0;
+            for (int k = 1; k <= K && i - k >= 0; ++k) {
+                curMax = max(curMax, A[i - k]);
+                best = max(best, dp[i - k] + curMax * k);
+            }
+            dp[i] = best; 
+        }
+        return dp[N];
+    }
+};
