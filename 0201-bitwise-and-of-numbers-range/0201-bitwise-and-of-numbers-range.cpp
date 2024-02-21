ce9ef233d13ecf932113c03e770451bcb58a2589
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-02-21 19:03:04 +0530

    Time: 8 ms (53.91%), Space: 8.8 MB (26.31%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,9 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+    return (n > m) ? (rangeBitwiseAnd(m/2, n/2) << 1) : m;
+}
+};
