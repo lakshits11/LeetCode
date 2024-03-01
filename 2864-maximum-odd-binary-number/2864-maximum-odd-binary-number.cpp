d2047d9e9a0294295a97118116acab53a7ee4bbf
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-03-01 18:55:54 +0530

    Time: 3 ms (77.44%), Space: 10.6 MB (9.26%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -0,0 +1,14 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        sort(s.rbegin(),s.rend());
+        for(int i = s.size()-1;i>=0;i--){
+            if(s[i]=='1')swap(s[i],s[s.size()-1]);
+        }
+        return s;
+    }
+};
