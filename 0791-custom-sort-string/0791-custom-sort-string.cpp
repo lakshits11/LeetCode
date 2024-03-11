fd7219a33c04193cbcdf2301acd2d506c2455ac3
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-03-11 18:48:29 +0530

    Time: 3 ms (50.27%), Space: 7.8 MB (48.22%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,32 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        std::unordered_map<char, int> charCount;
+        for (char c : order) {
+            charCount[c] = 0;
+        }
+        
+        for (char c : s) {
+            if (charCount.find(c) != charCount.end()) {
+                charCount[c]++;
+            }
+        }
+        
+        std::string sortedS;
+        for (char c : order) {
+            sortedS.append(charCount[c], c);
+        }
+        
+        for (char c : s) {
+            if (charCount.find(c) == charCount.end()) {
+                sortedS.push_back(c);
+            }
+        }
+        
+        return sortedS;
+    }
+};
