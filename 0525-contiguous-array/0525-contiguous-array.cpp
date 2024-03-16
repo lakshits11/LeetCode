369684e0fc47ea07d86bbf9ca21b576cea8eb319
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-03-16 15:38:51 +0530

    Time: 69 ms (93.66%), Space: 87.7 MB (53.36%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,18 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int sum=0, maxLen=0;
+        unordered_map<int, int> seen{{0, -1}};
+        
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i]==1 ? 1 : -1;
+            if(seen.count(sum)) maxLen = max(maxLen, i-seen[sum]);
+            else seen[sum] = i;
+        }
+        return maxLen;
+    }
+};
