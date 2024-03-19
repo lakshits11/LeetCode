33e42972f27f93e55c29b529ee0aaacc03ef6644
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-03-19 19:12:06 +0530

    Time: 34 ms (99.08%), Space: 38 MB (65.52%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,25 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n)
+    {
+        unordered_map<char, int> mp;
+        int count = 0;
+        for(char &c : tasks)
+        {
+            mp[c]++;
+            count = max(count, mp[c]);
+        }
+        
+        int ans = (count-1)*(n+1);
+        for(pair<int, int> x : mp)
+        {
+            if(x.second == count)
+                ans++;
+        }
+        return max((int)tasks.size(), ans);
+    }
+};
