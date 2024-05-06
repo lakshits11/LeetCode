bde917773635cc4724640e03376633226fc24e73
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-05-06 19:35:31 +0530

    Time: 187 ms (99.91%), Space: 164.2 MB (75.84%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+class Solution {
+public:
+ListNode* removeNodes(ListNode* head) {
+        if (!head) return NULL;
+        head->next = removeNodes(head->next);
+        return head->next && head->val < head->next->val ?  head->next : head;
+    }
+
+};
