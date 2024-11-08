49e9ca59ede909046baddb6b83ef26498d2edce9
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-11-08 19:12:15 +0530

    Time: 32 ms (49.36%), Space: 60.3 MB (13.76%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,18 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int n = candidates.size(),ans = 0;
+        for(int i=0;i<32;i++){
+            int cnt = 0;
+            for(auto candidate : candidates){
+                if(candidate & (1<<i))cnt++;
+            }
+            ans = max(ans,cnt);
+        }
+        return ans;
+    }
+};
