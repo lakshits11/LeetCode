5d5dedd86c4ebb885944718c609212bcfaee38b3
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-11-08 19:05:20 +0530

    Time: 0 ms (100.00%), Space: 95.3 MB (82.32%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -0,0 +1,15 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = size(nums), XOR = 0;
+        vector<int> ans(n);
+        for(auto& num : nums) XOR ^= num;
+        ans[0] = XOR ^= (1 << maximumBit) - 1;  // 2^maximumBit - 1
+        for(int i = 1; i < n; i++)
+            ans[i] = XOR ^= nums[n - i];            
+        return ans;
+    }
+};
