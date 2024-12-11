6f0006cfa9cc18a9f07e2ccfd21ec100e186e039
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-12-11 21:39:53 +0530

    Time: 550 ms (20.85%), Space: 191.6 MB (27.13%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -2,21 +2,42 @@ static const auto speedup = []() -> int{
     std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
 }();
 
+// class Solution {
+// public:
+//     int maximumBeauty(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> pref(3*100002, 0);
+//         for(int i = 0; i < n; ++i)
+//         {
+//             pref[nums[i]-k+100000]++;
+//             pref[nums[i]+k+100001]--;
+//         }
+//         int maxx = pref[0];
+//         for(int i=1;i<pref.size();++i)
+//         {
+//             pref[i]+=pref[i-1];
+//             maxx = max(maxx, pref[i]);
+//         }
+//         return maxx;
+//     }
+// };
+
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> pref(3*100002, 0);
+        int maxx = 0;
+        map<int, int> pref;
         for(int i = 0; i < n; ++i)
         {
-            pref[nums[i]-k+100000]++;
-            pref[nums[i]+k+100001]--;
+            pref[nums[i]-k]++;
+            pref[nums[i]+k+1]--;
         }
-        int maxx = pref[0];
-        for(int i=1;i<pref.size();++i)
+        int prev = 0;
+        for(auto &x : pref)
         {
-            pref[i]+=pref[i-1];
-            maxx = max(maxx, pref[i]);
+            prev += x.second;
+            maxx = max(maxx, prev);
         }
         return maxx;
     }
