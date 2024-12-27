5a5b2e473fd806a37366e563791546600d7da942
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2024-12-27 13:04:12 +0530

    Time: 0 ms (100.00%), Space: 46 MB (33.05%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,17 +1,16 @@
-class Solution
-{
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
+class Solution {
 public:
-    int maxScoreSightseeingPair(vector<int> &a)
+    int maxScoreSightseeingPair(vector<int>& a)
     {
-        ios_base::sync_with_stdio(false);
-        int n = a.size();
-        int maxEnd = a[n - 1] - (n - 1);
-        int maxOverAllGain = INT_MIN;
-        for (int i = n - 2; i >= 0; i--)
-        {
-            maxEnd = max(maxEnd, a[i + 1] - i - 1);
-            maxOverAllGain = max(maxOverAllGain, a[i] + i + maxEnd);
+        int n = a.size(), maxScore = 0;
+        int maxLeft = a[0] + 0;
+        for(int j = 1; j < n; j++) {
+            maxScore = max(maxScore, maxLeft + a[j] - j);
+            maxLeft = max(maxLeft, a[j] + j);
         }
-        return maxOverAllGain;
+        return maxScore;
     }
 };
