8202c61b4208e58ed815777bad0c9cd7860eaeca
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-01-12 10:08:27 +0530

    Time: 15 ms (51.52%), Space: 30.2 MB (34.26%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,29 +1,26 @@
-// https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/discuss/1650613/Intuition-Explained-oror-Balanced-Parentheses-Greedy-Approach-oror-C++-Clean-Code
-
 static const auto speedup = []() -> int{
     std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
 }();
-
 class Solution {
 public:
-    // locked => lk
-    bool canBeValid(string s, string lk) {
-        int b = 0; // balance
+    bool canBeValid(string s, string locked) {
         int n = s.size();
         if(n&1) return false;
-        
-        for(int i = 0; i < n; i++)
+        int bal = 0;
+        for(int i = 0; i < n; ++i)
         {
-            if(s[i]=='(' || lk[i]=='0') b++;
-            else b--;
-            if(b<0) return false;
+            if(locked[i]=='0' || s[i]=='(') bal++;
+            else bal--;
+
+            if(bal < 0) return false;
         }
-        b=0;
+        bal = 0;
         for(int i = n-1; i >= 0; --i)
         {
-            if(s[i]==')' || lk[i]=='0') b++;
-            else b--;
-            if(b<0) return false;
+            if(locked[i]=='0' || s[i]==')') bal++;
+            else bal--;
+
+            if(bal < 0) return false;
         }
         return true;
     }
