b245b903511f4648f9b01dd8dfa92a30afa02ac5
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-01-19 11:03:27 +0530

    Time: 0 ms (100%), Space: 26.1 MB (46.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,37 @@
-static const auto speedup = []() -> int{
-    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+// https://leetcode.com/problems/trapping-rain-water/solutions/17357/sharing-my-simple-c-code-o-n-time-o-1-space
+
+// basically we want to calculate amount of water  held above each container
+// this will depend on first greater height container on its left side and on
+// right side
+
+static const auto speedup = []() -> int {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
 }();
 class Solution {
 public:
     int trap(vector<int>& ht) {
         int n = ht.size();
-        int l = 0, r = n-1, ml = 0, mr = 0;
+        // ml = maximumLeft, mr = maximumRight
+        int l = 0, r = n - 1, ml = 0, mr = 0;
         int ans = 0;
-        while(l <= r)
+        while (l <= r) 
         {
-            if(ht[l] <= ht[r])
+            if (ht[l] <= ht[r]) 
             {
-                if(ml < ht[l])
+                if (ht[l] > ml)
                     ml = ht[l];
-                else ans += ml-ht[l];
+                else
+                    ans += ml - ht[l];
                 l++;
-            }
-            else {
-                if(mr < ht[r]) mr = ht[r];
-                else ans += mr-ht[r];
+            } 
+            else 
+            {
+                if (ht[r] > mr)
+                    mr = ht[r];
+                else
+                    ans += mr - ht[r];
                 r--;
             }
         }
