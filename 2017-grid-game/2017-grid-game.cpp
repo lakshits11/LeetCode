c965776ce96441ab4c29d0a271eb02651a203ed7
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-01-21 20:46:49 +0530

    Time: 0 ms (100%), Space: 66.9 MB (53.39%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,13 +1,16 @@
+static const auto speedup = []() -> int{
+    std::ios::sync_with_stdio(false);std::cin.tie(nullptr);return 0;
+}();
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& g) {
-        long long top = accumulate(g[0].begin(), g[0].end(), 0ll);
-        long long bottom = 0, res = LLONG_MAX;
-        for(int i = 0; i < g[0].size(); i++)
+        long long top = accumulate(begin(g[0]), end(g[0]), 0ll);
+        long long bottom = 0;
+        long long res = LLONG_MAX;
+        for(int i = 0; i < g[0].size(); ++i) 
         {
             top -= g[0][i];
-            long long x = max(top, bottom);
-            res = min(res, x);
+            res = min(res, max(top, bottom));
             bottom += g[1][i];
         }
         return res;
