f2d929543bb78b859caf9ccea63d9c74d99879bb
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-02-27 21:14:42 +0530

    Time: 172 ms (14.94%), Space: 13 MB (64.08%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -1,17 +1,24 @@
+// https://leetcode.com/problems/maximum-compatibility-score-sum/solutions/1360805/backtracking-with-stl-10-lines-of-code-c
 class Solution {
 public:
-    int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) 
+    int maxCompatibilitySum(vector<vector<int>>& st, vector<vector<int>>& mt) 
     {
         int ans = 0;
-        vector<int> pos;
-        for(int i=0;i<students.size();i++) pos.push_back(i);
-        do{
+        vector<int> pos(st.size());
+        for(int i=0;i<st.size();++i)
+            pos[i]=i;
+        do {
             int curr = 0;
-            for(int i = 0;i<students.size(); i++)
-                for(int j=0;j<students[pos[i]].size();j++)
-                    curr += (students[pos[i]][j] == mentors[i][j]);
+            for(int i = 0; i < st.size(); ++i)
+            {
+                for(int j = 0; j < st[pos[i]].size(); ++j)
+                {
+                    curr += (st[pos[i]][j] == mt[i][j]);
+                }   
+            }
             ans = max(ans, curr);
-        } while(next_permutation(pos.begin(), pos.end()) );
+        } while (next_permutation(pos.begin(), pos.end()));
+        
         return ans;
     }
 };
