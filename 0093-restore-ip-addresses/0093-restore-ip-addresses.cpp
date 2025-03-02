a1a6dd217b9b8270766bc02ef94b82e797714227
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-03-02 12:46:25 +0530

    Time: 0 ms (100%), Space: 9.6 MB (80.49%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,43 +1,35 @@
-bool isValid(string s)
-{
-    if (s.length() > 3 || s.empty())
-        return false;
+class Solution {
+private:
+    bool isValid(string &s) {
+        if (s.length() > 3 || s.empty())
+            return false;
 
-    // 0 is valid but starting with 0 and length greater than 1, like 00, 01, 023...arenot valid.
-    if (s[0] == '0' and s.length() > 1)
-        return false;
+        // 0 is valid, but numbers like "00", "01", "023" are not valid.
+        if (s[0] == '0' && s.length() > 1)
+            return false;
 
-    int val = stoi(s);
+        int val = stoi(s);
+        return (val >= 0 && val <= 255);
+    }
 
-    return (val >= 0 and val <= 255);
-}
-
-class Solution
-{
 public:
-    vector<string> restoreIpAddresses(string s)
-    {
+    vector<string> restoreIpAddresses(string s) {
         vector<string> result;
-
         int n = s.length();
-        if (n >= 13)
+
+        if (n < 4 || n > 12) // Pruning: Valid IPs must have 4 to 12 characters
             return {};
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                for (int k = j + 1; k < n; k++)
-                {
+        for (int i = 1; i <= 3 && i < n; i++) { // 1st partition
+            for (int j = i + 1; j <= i + 3 && j < n; j++) { // 2nd partition
+                for (int k = j + 1; k <= j + 3 && k < n; k++) { // 3rd partition
                     string a = s.substr(0, i);
                     string b = s.substr(i, j - i);
                     string c = s.substr(j, k - j);
                     string d = s.substr(k);
 
-                    if (isValid(a) and isValid(b) and isValid(c) and isValid(d))
-                    {
-                        string ip = a + "." + b + "." + c + "." + d;
-                        result.push_back(ip);
+                    if (isValid(a) && isValid(b) && isValid(c) && isValid(d)) {
+                        result.push_back(a + "." + b + "." + c + "." + d);
                     }
                 }
             }
