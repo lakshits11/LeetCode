7c02969d29a16d9ea36d10326b5f3ced46a819e2
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2025-11-22 17:07:22 +0530

    Time: 0 ms (100%), Space: 23.1 MB (97.64%)

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three-11-22-2025-17-07-24.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ans = 0;
+        for(int i : nums)
+        {
+            if(i%3) ans++;
+        }
+        return ans;
+    }
+};
