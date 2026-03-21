7dd4ba43d1c4e98f13d0570de5f4ae16287e0c96
Lakshit Somani <54276661+lakshits11@users.noreply.github.com>
2026-03-21 10:20:40 +0530

    Time: 0 ms (100%), Space: 33.8 MB (60.84%)

---
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically-03-21-2026-10-20-41.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        // Flip the k x k submatrix vertically
+        for (int i = 0; i < k / 2; i++) {
+            for (int j = 0; j < k; j++) {
+                swap(grid[x + i][y + j], grid[x + k - 1 - i][y + j]);
+            }
+        }
+        return grid;
+    }
+};
